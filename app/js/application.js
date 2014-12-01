5c7d1a73a3fb375c69634c2131ff18ff7c5095b8
Tommy Barth <tommyhbrc@gmail.com>
2014-11-30 22:49:21 -0800

    Adds application initializer to persist user and auth with token

--- app/js/application.js
+++ app/js/application.js
@@ -7,3 +7,18 @@ BatchMaker.ApplicationAdapter = DS.FirebaseAdapter.extend({
 });
 
 filepicker.setKey("AqA4hQVEeS3qtLs2vjBRJz");
+
+Ember.Application.initializer({
+  name: 'firebase-session',
+
+  initialize: function(container, application){
+    application.deferReadiness();
+    var token = localStorage.getItem('batchmaker-firebase-token');
+    if (token) {
+      var session = container.lookup('controller:session');
+      session.authWithToken(token).then(function(){
+        application.advanceReadiness();
+      });
+    } else {application.advanceReadiness();}
+    }
+  });
