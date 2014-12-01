0d2b01f1a0c663e05fe4861e17d68f0943c55e11
Tommy Barth <tommyhbrc@gmail.com>
2014-12-01 12:53:05 -0800

    Fixes unless for public recipes, adds private recipes

--- app/js/controllers/recipes_show_controller.js
+++ app/js/controllers/recipes_show_controller.js
@@ -1,6 +1,7 @@
 BatchMaker.RecipesShowController = Ember.ObjectController.extend({
   needs: ['application'],
   currentRecipe: Ember.computed.alias('model'),
+  
   imgStyle: function(){
     return new Ember.Handlebars.SafeString("background-image: url('"+this.get('imgUrl')+"')").toString();
   }.property('imgUrl')
